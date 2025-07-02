e57f319fb75e56b83e0d98c0d475f55a0c7b11f0
Your Name <you@example.com>
2025-07-02 22:41:11 +0300

    Initial Commit

---
+++ omnixr-measure/omnixr_measureApp.swift
@@ -0,0 +1,17 @@
+//
+//  omnixr_measureApp.swift
+//  omnixr-measure
+//
+//  Created by phones luxury on 02/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct omnixr_measureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ omnixr-measureTests/omnixr_measureTests.swift
@@ -0,0 +1,17 @@
+//
+//  omnixr_measureTests.swift
+//  omnixr-measureTests
+//
+//  Created by phones luxury on 02/07/2025.
+//
+
+import Testing
+@testable import omnixr_measure
+
+struct omnixr_measureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
